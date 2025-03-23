527347df0be77a0b6307ce1a471b6634be740cbe
Stropheum <stropheum060@gmail.com>
2025-03-23 14:33:52 -0400

    default setup needler class

--- Source/HallCrawl/NeedlerProjectile.cpp
+++ Source/HallCrawl/NeedlerProjectile.cpp
@@ -1,25 +1,17 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
+﻿#include "NeedlerProjectile.h"
 
 
-#include "NeedlerProjectile.h"
-
-
-// Sets default values
 ANeedlerProjectile::ANeedlerProjectile()
 {
-	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 }
 
-// Called when the game starts or when spawned
 void ANeedlerProjectile::BeginPlay()
 {
 	Super::BeginPlay();
-	
 }
 
-// Called every frame
-void ANeedlerProjectile::Tick(float DeltaTime)
+void ANeedlerProjectile::Tick(const float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 }
